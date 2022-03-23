8783de5a6f1f882716d0b426d0bc34c8e0bd15f2
Jarle Syvertsen <wolfinabarrel@protonmail.com>
2022-03-23 14:42:51 +0100

    Updated root and schema to be a bit more complete.

--- server.js
+++ server.js
@@ -7,19 +7,72 @@ const app = express();
 
 const schema = buildSchema(`
  type Query {
-     description: String
-     price: Float
+    products: [Product]
+    orders: [Order]
  }
+
+ type Product {
+     id: ID!
+     description: String!
+     reviews: [Review]
+     price: Float!
+ }
+ 
+ type Review {
+    rating: Int!
+    comment: String
+ }
+ type Order {
+     date: String!
+     subtotal: Float!
+     items: [OrderItem]
+ }
+
+ type OrderItem {
+     product: Product!
+     quantity: Int!
+ }
+
 `);
 
 const root = {
-    description: 'Test shoe',
-    price: 42.69 
+    products: [
+    {
+        id: 'redshoe',
+        description: 'Red Shoe',
+        price: 42.69,
+    },
+    
+    {
+        id: 'bluebean',
+        description: 'It\'s a blue bean',
+        price: 69.42
+    }
+ ],
+    orders: [ 
+    {
+        date: '2005-05-05',
+        subtotal: 111.11,
+        items: [ 
+          {
+            product: {
+                id: 'redshoe',
+                description: 'Somewhat old and possibly haunted shoe',
+                price: 2.1
+            },
+            quantity: 3,
+          }
+        ]
+    }
+    
+    
+ ]
 }
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     rootValue: root,
+    graphiql: true
 }));
 
 app.listen(8000, () => {
