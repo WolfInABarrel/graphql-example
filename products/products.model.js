8edbf15f1e94a6bad13f33673cbd20db8b2074a7
Jarle Syvertsen <wolfinabarrel@protonmail.com>
2022-03-23 19:23:23 +0100

    Exercise addProductReview (tested ok, not compared to school material.)

--- products/products.model.js
+++ products/products.model.js
@@ -3,12 +3,14 @@ const products = [
         id: 'redshoe',
         description: 'Red Shoe',
         price: 42.69,
+        reviews: []
     },
     
     {
         id: 'bluebean',
         description: 'It\'s a blue bean',
-        price: 69.42
+        price: 69.42,
+        reviews: []
     }
  ]
 
@@ -16,4 +18,42 @@ function getAllProducts() {
     return products;
 }
 
- module.exports = { getAllProducts };
+function getProductsByPrice(min, max) {
+    return products.filter((product) => {
+        return product.price >= min && product.price <= max;
+    })
+}
+
+function getProductByID(id) {
+    return products.find((product) => {
+       return products.id === id;
+    })
+}
+
+function addNewProduct(id, description, price) {
+    const newProduct = {
+        id,
+        price,
+        description,
+        reviews: []
+    };
+
+    products.push(newProduct);
+    return newProduct;
+}
+
+function addNewProductReview(id, rating, comment) {
+    const newReview = {
+        rating,
+        comment
+    }
+    const productToAppend = products.find((product) => {
+        return product.id === id;
+    })
+    productToAppend.reviews.push(newReview);
+    return newReview;
+
+}
+
+
+ module.exports = { getAllProducts, getProductsByPrice, getProductByID, addNewProduct, addNewProductReview };
--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -6,6 +6,20 @@ const productsResolver = {
             console.log('Getting the products.');
             return productsModel.getAllProducts();
         },
+        productsByPrice: (_, args) => {
+            return productsModel.getProductsByPrice(args.min, args.max);
+        },
+        productByID: (_, args) => {
+            return productsModel.getProductByID(args.id);
+        }   
+    },
+    Mutation: {
+        addNewProduct: (_, args) => {
+           return productsModel.addNewProduct(args.id, args.description, args.price);
+        },
+        addNewProductReview: (_, args) => {
+            return productsModel.addNewProductReview(args.id, args.rating, args.comment);
+        }
     }
 }
 
